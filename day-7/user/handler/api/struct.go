48533d23ff671fd6f6e60dd2bc802408da83f6e1
Jiran03 <jiranmuhammad7@gmail.com>
2022-09-21 20:15:04 +0800

    added task day 7

---
+++ day-7/user/handler/api/struct.go
@@ -0,0 +1,56 @@
+package handlerAPI
+
+import (
+	"time"
+
+	helperTime "github.com/Jiran03/agmc/task/day5/helpers/time"
+	"github.com/Jiran03/agmc/task/day5/user/domain"
+)
+
+type RequestJSON struct {
+	Name     string `json:"name" form:"name" validate:"required"`
+	Email    string `json:"email" form:"email" validate:"required,email"`
+	Password string `json:"password" form:"password" validate:"required,min=8"`
+}
+
+type RequestLoginJSON struct {
+	Email    string `json:"email" form:"email" validate:"required,email"`
+	Password string `json:"password" form:"password" validate:"required,min=8"`
+}
+
+type Token struct {
+	Token string `json:"token"`
+}
+
+func toDomain(req RequestJSON) domain.User {
+	return domain.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: req.Password,
+	}
+}
+
+type ResponseJSON struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name" form:"name"`
+	Email string `json:"email" form:"email"`
+	// Gender      string    `json:"gender" form:"gender"`
+	Status    string    `json:"status" form:"status"`
+	CreatedAt time.Time `json:"created_at" form:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" form:"updated_at"`
+}
+
+func fromDomain(domain domain.User) ResponseJSON {
+	//parse unix timestamp to time.Time
+	tmCreatedAt := helperTime.NanoToTime(domain.CreatedAt)
+	tmUpdatedAt := helperTime.NanoToTime(domain.UpdatedAt)
+
+	return ResponseJSON{
+		ID:    domain.ID,
+		Name:  domain.Name,
+		Email: domain.Email,
+		// Gender:    domain.Gender,
+		CreatedAt: tmCreatedAt,
+		UpdatedAt: tmUpdatedAt,
+	}
+}
